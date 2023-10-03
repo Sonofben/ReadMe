f64381b2aef984040dedcb2d041f0ed1ecf3f368
Joseph Benson <bensonjosepholajide11@gmail.com>
2023-10-03 11:31:43 +0100

    Just Me showing off

---
+++ ShowMe.js
@@ -0,0 +1,38 @@
+<img align='center' width='1020' src='https://img.freepik.com/free-vector/teenager-boy-bedroom-interior-computers-desk_107791-2738.jpg?w=826&t=st=1665971325~exp=1665971925~hmac=ec0dfa2d7e5eab170131ade718501cc66f0eeb07495ca493d38c092b518cb8a9'>
+
+    ## Joseph Benson
+
+    👨‍💻 Technical Support Specialist | 🚀 Software Developer | 🐍 Python | 🖥️ C++ | 🌐 JavaScript
+
+    Welcome to my profile! I'm Joseph Benson, a versatile professional with expertise in technical support and software development. My skills span Python, C++, and JavaScript coding. I'm passionate about solving problems, creating software solutions, and continuously learning in the ever-evolving tech world. Let's connect and collaborate!
+
+    - 🔭 I’m currently an [Intern student in the ALX SE Program](https://www.alxafrica.com/software-engineering-2023/)
+    - 🤝 I'm an experineced IT Support Specalist
+    - 🌱 I’m currently learning **Python(Flask) and JavaScript (React )**
+    - 👯 I’m looking to collaborate on **Python,C++,Javascript Projects**
+    - 🤝 I’m looking for more experience with projects and internship opportunities in becoming a better SE
+    - 💬 Ask me about **Python, C++, MySQL, Javascript, Tech Support, ERP System, Database Management , Data Anaylsis, Azure, and Workspace Migration**
+    - 📫 You can reach me via **eliteben**
+
+    <h3 align="left">Connect with me:</h3>
+    <p align="left">
+        <a href="https://twitter.com/Black_King1108" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="Black_King1108" height="30" width="40" /></a>
+        <a href="https://www.linkedin.com/in/joseph-benson-74b831224/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="Joseph Benson" height="30" width="40" /></a>
+    </p>
+
+    <img alt="writing codes" width='1020' height='200' align="center" src="https://media1.giphy.com/media/PI3QGKFN6XZUCMMqJm/giphy.gif?cid=ecf05e47o2ggol62o8sbe63ur5tr90j2vv3z7cs7am4if89k&rid=giphy.gif&ct=g">
+
+        <h3 align="left">Languages and Tools:</h3>
+        <p align="left">
+            <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40" height="40"/></a>
+            <a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer"><img src="https://cdn.worldvectorlogo.com/logos/django.svg" alt="django" width="40" height="40"/></a>
+            <a href="https://www.figma.com/" target="_blank" rel="noreferrer"><img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40"/></a>
+            <a href="https://flask.palletsprojects.com/" target="_blank" rel="noreferrer"><img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" alt="flask" width="40" height="40"/></a>
+            <a href="https://git-scm.com/" target="_blank" rel="noreferrer"><img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/></a>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/></a>
+            <a href="https://www.linux.org/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/></a>
+            <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/></a>
+            <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/></a>
+            <a href="https://www.python.org" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/></a>
+            <a href="https://www.vagrantup.com/" target="_blank" rel="noreferrer"><img src="https://www.vectorlogo.zone/logos/vagrantup/vagrantup-icon.svg" alt="vagrant" width="40" height="40"/></a>
+        </p>
